8bb69370afe9ff3c99bb5967925e292050468535
wuchupeng <wuchupeng@lizhi.fm>
2019-11-04 21:09:46 +0800

    TCP服务器与客服端互发消息

---
+++ socket_learn2_0/src/main/java/chapter5_1/clink/net/qiujuer/clink/Clink.java
@@ -0,0 +1,4 @@
+package chapter5_1.clink.net.qiujuer.clink;
+
+public class Clink {
+}
---
+++ socket_learn2_0/src/main/java/chapter5_1/constants/TCPConstants.java
@@ -0,0 +1,6 @@
+package chapter5_1.constants;
+
+public class TCPConstants {
+    // 服务器固化UDP接收端口
+    public static int PORT_SERVER = 30401;
+}
